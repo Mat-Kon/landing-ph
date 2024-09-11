cf155e9f263dcda7573a68a067fa11710f08e78a
Mat-Kon <mat_kon@mail.ru>
2024-09-11 22:06:43 +0300

    init: install dependencies, create html scelet

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'img/[name][ext]',
+  },
+  devServer: {
+    compress: false,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new TerserPlugin(),
+  ],
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  externals: {
+    ace: 'ace',
+  },
+};
